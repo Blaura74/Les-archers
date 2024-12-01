60db31cde937424bb8bf16961f9efbf69ade6fd8
Laura Berthoud <lauraberthoud74@gmail.com>
2024-12-01 22:40:20 +0100

    creation aside.php et mise a jour style.css

---
+++ aside.php
@@ -0,0 +1,60 @@
+<aside>
+                <!-- implementation widget weather-->
+                <article>
+                    <h3>Météo</h3>
+                    <div id="weather">
+                        <iframe id="widget_autocomplete_preview" frameborder="0"
+                            src="https://meteofrance.com/widget/prevision/742080##F4970A"
+                            title="Prévisions Passy par Météo-France"> </iframe>
+                    </div>
+                </article>
+                <!-- implementation calendar-->
+                <article>
+                    <h3>Calendrier</h3>
+                    <div id="calendar">
+                        <iframe
+                            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23F6BF26&ctz=Europe%2FParis&showNav=1&showDate=1&showPrint=0&showCalendars=0&showTz=0&src=YXJjaGVyc2R1bW9udGJsYW5jQGdtYWlsLmNvbQ&color=%23616161"
+                            style="border:solid 1px #777" width="300" height="310" frameborder="0"></iframe>
+                    </div>
+                </article>
+                <!--implementation slide partenaires-->
+
+                <article id="partnership">
+                    <h3>Nos partenaires</h3>
+                    <div class="center">
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <a href="https://archerystudio.wixsite.com/archerystudio/l-equipe"
+                                        title="Archery studio" target="_blank">
+                                        <img src="img\logos\archerystudio.png" class="d-block w-100"
+                                            alt="logo Archery studio">
+                                    </a>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="img\logos\tramoy.png" class="d-block w-100" alt="logo société tramoy">
+                                </div>
+                                <div class="carousel-item">
+                                    <a href="https://www.ffta.fr/" title="FFTA">
+                                        <img src="img\logos\FFTA1.jpg" class="d-block w-100"
+                                            alt="Logo fédération française de tir à l'arc">
+                                    </a>
+                                </div>
+                                <div class="carousel-item">
+                                    <a href="https://www.ville-passy-mont-blanc.fr/" title="ville de Passy">
+                                        <img src="img\logos\Passy.png" class="d-block w-100" alt="logo ville de passy">
+                                    </a>
+                                </div>
+                                <div class="carousel-item">
+                                    <a href="https://www.auvergnerhonealpes.fr/" title="Région Auvergne-Rhône-Alpes">
+                                        <img src="img\logos\region.jpg" class="d-block w-100"
+                                            alt="logo région auvergnes rhone alpes">
+                                    </a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </article>
+            </aside>
--- index.php
+++ index.php
@@ -37,99 +37,38 @@
 <main>
 
 
-</div>
-
-<div class="content">
-    <div class="principale">
-        <article class="bienvenue">
-            <h1>Bienvenue sur notre site !</h1>
-            <p> Le club des Archers du Mont Blanc est un club de tir à l'arc situé à Passy en Haute Savoie(74). 
-                Notre club est né le 17 mars 2022 et compte aujourd’hui 31 licenciés.
-                Nous pratiquons le tir à l’arc dans différentes disciplines comme le tir en salle (saison hiver)
-                et le
-                tir à l'arc en extérieur (saison
-                printemps/été) qui se nomme également TAE. Nos entrainements s'effectuent au boulodrome de
-                passy.
-                Mais
-                vous pouvez nous retrouver sur
-                differents évènements tout au long de l'année.
-                </p>
-        </article>
-        <article class="actualites">
-            <h2>Dernières news du club !</h2>
-            <iframe class="fb"
-                src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Farchersmontblanc%2Fposts%2Fpfbid02gKt91DKfKFxdxbdFQkbZz6pBqm43hbYdRAEM6cGvApqUDLXqKjV89C7WQy27teqAl&show_text=true&width=500"
-                width="500" height="652" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-                allowfullscreen="true"
-                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-            <iframe class="fb"
-                src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Farchersmontblanc%2Fposts%2Fpfbid08z5h8GJFsdRnCsk2UtFrFVq9FfSs1EE3m6QdDjiBSJWD2YyTXNUrNwaiVJB1QrRQl&show_text=true&width=500"
-                width="500" height="640" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-                allowfullscreen="true"
-                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-        </article>
-        
-    </div>
-    <aside>
-                <!-- implementation widget weather-->
-                 <article>
-                    <h3>Météo</h3>
-                <div id="weather">
-                    <iframe id="widget_autocomplete_preview" frameborder="0"
-                        src="https://meteofrance.com/widget/prevision/742080##F4970A"
-                        title="Prévisions Passy par Météo-France"> </iframe>
-                </div>
-                 </article>
-                <!-- implementation calendar-->
-                 <article>
-                    <h3>Calendrier</h3>
-                <div id="calendar">
-                    <iframe
-                        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23F6BF26&ctz=Europe%2FParis&showNav=1&showDate=1&showPrint=0&showCalendars=0&showTz=0&src=YXJjaGVyc2R1bW9udGJsYW5jQGdtYWlsLmNvbQ&color=%23616161"
-                        style="border:solid 1px #777" width="300" height="310" frameborder="0"></iframe>
-                </div>
+    <div class="content">
+        <div class="principale">
+            <article class="bienvenue">
+                <h1>Bienvenue sur notre site !</h1>
+                <p> Le club des Archers du Mont Blanc est un club de tir à l'arc situé à Passy en Haute Savoie(74). 
+                    Notre club est né le 17 mars 2022 et compte aujourd’hui 31 licenciés.
+                    Nous pratiquons le tir à l’arc dans différentes disciplines comme le tir en salle (saison hiver)
+                    et le
+                    tir à l'arc en extérieur (saison
+                    printemps/été) qui se nomme également TAE. Nos entrainements s'effectuent au boulodrome de
+                    passy.
+                    Mais
+                    vous pouvez nous retrouver sur
+                    differents évènements tout au long de l'année.
+                    </p>
             </article>
-                <!--implementation slide partenaires-->
-
-                <article id="partnership">
-                    <h3>Nos partenaires</h3>
-                    <div class="center">
-                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-inner">
-                               <!-- <div class="carousel-item active">
-                                    <a href="https://archerystudio.wixsite.com/archerystudio/l-equipe"
-                                        title="Archery studio" target="_blank" >
-                                        <img src="img/archerystudio.png" class=" d-block w-100"
-                                            alt="logo Archery studio">
-                                    </a>
-                                </div> -->
-                                <div class="carousel-item active">
-                                    <img src="img\logos\tramoy.png" class=" d-block w-100" alt="logo société tramoy">
-                                </div>
-                                <div class="carousel-item">
-                                    <a href="https://www.ffta.fr/" title="FFTA" target="_blank">
-                                        <img src="img\logos\FFTA1.jpg" class=" d-block w-100"
-                                            alt="Logo fédération française de tir à l'arc">
-                                    </a>
-                                </div>
-                                <div class="carousel-item">
-                                    <a href="https://www.ville-passy-mont-blanc.fr/" title="ville de Passy" target="_blank">
-                                        <img src="img\logos\Passy.png" class=" d-block w-100" alt="logo ville de passy">
-                                    </a>
-                                </div>
-                                <div class="carousel-item">
-                                    <a href="https://www.auvergnerhonealpes.fr/" title="Région Auvergne-Rhône-Alpes" target="_blank">
-                                        <img src="img\logos\region.jpg" class="d-block w-100"
-                                            alt="logo région auvergnes rhone alpes">
-                                    </a>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-                </article>
-            </aside>
+            <article class="actualites">
+                <h2>Dernières news du club !</h2>
+                <iframe class="fb"
+                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Farchersmontblanc%2Fposts%2Fpfbid02gKt91DKfKFxdxbdFQkbZz6pBqm43hbYdRAEM6cGvApqUDLXqKjV89C7WQy27teqAl&show_text=true&width=500"
+                    width="500" height="652" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                    allowfullscreen="true"
+                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                <iframe class="fb"
+                    src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Farchersmontblanc%2Fposts%2Fpfbid08z5h8GJFsdRnCsk2UtFrFVq9FfSs1EE3m6QdDjiBSJWD2YyTXNUrNwaiVJB1QrRQl&show_text=true&width=500"
+                    width="500" height="640" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                    allowfullscreen="true"
+                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+            </article>
+            
+        </div>
+            <?php include('aside.php'); ?>
 
         </div>
 
